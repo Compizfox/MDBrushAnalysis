3cff17f82f0f33eeab6f170b2a6b1f7d909859fb
lars <lars@tuxplace.nl>
2019-10-21 14:39:32 +0200

    Initial commit

---
+++ BrushDensityParser.py
@@ -0,0 +1,120 @@
+"""
+Exports the AveChunkParser and BrushDensityParser classes.
+"""
+
+import re
+from io import StringIO
+from typing import Sequence, Optional
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+class AveChunkParser:
+	"""
+	Parses data outputted by LAMMPS' ave/chunk or ave/time (in vector mode) fixes.
+	"""
+	@staticmethod
+	def _load_file(filename: str, cols: Optional[Sequence]) -> np.ndarray:
+		"""
+		:param cols:
+		:param filename:
+		:return:
+		"""
+		# Use regex to strip the timestep lines
+		with open(filename) as f:
+			p = re.compile(r'\n\d.*')
+			string = p.sub('', f.read())
+
+		return np.loadtxt(StringIO(string), usecols=cols)
+
+	@classmethod
+	def get_reshaped_data(cls, filename: str, cols: Optional[Sequence]) -> np.ndarray:
+		"""
+
+		:param filename:
+		:param cols:
+		:return:
+		"""
+		data = cls._load_file(filename, cols)
+
+		# The data array is a '2D flattened' representation of a 3D array
+		# (the third dimension being the time). We need to first get the number of
+		# chunks and then reshape the array using that number
+
+		# Get the index where the second chunk is 1 (that is the number of chunks)
+		num_chunks = np.nonzero(data[:, 0] == 1)[0][1]
+
+		return data.reshape((-1, num_chunks, len(cols)))
+
+
+class BrushDensityParser(AveChunkParser):
+	"""
+	Parses a LAMMPS ave/chunk output file.
+	"""
+
+	@classmethod
+	def load_density(cls, filename: str) -> np.ndarray:
+		"""
+		:param filename: String containing the filename of the density file to parse.
+		:return: A 3d ndarray with shape (a, b, 3), representing density profiles at different timesteps with a being
+				 the number of temporal profiles and b being the number of spatial chunks in a profile. One row is a
+				 tuple of (chunk #, spatial distance, density).
+		"""
+		return cls.get_reshaped_data(filename, (0, 1, 3))
+
+
+class ProfilePlotter:
+	"""
+	Plots a profile (arbitrary quantity vs distance)
+	"""
+	def __init__(self, profile: np.ndarray, trim: int = None):
+		self.profile = profile
+		self.TRIM = (trim or 15)
+		self.profile_ta = np.mean(self.profile[self.TRIM:, :, :], axis=0)
+
+	def plot_ta(self, ax):
+		"""
+		Plot time-averaged profile.
+		:param ax:
+		:return:
+		"""
+		ax.plot(self.profile_ta[:, 1], self.profile_ta[:, 2])
+		ax.set_xlabel('Distance ($\sigma$)')
+		ax.set_ylim(0)
+		ax.grid()
+
+	def plot_animation(self, fig, quantity: str = 'Y'):
+		"""
+
+		:param fig:
+		:param quantity:
+		:return:
+		"""
+		# Gather some sizes/maximums/sums in several dimensions
+		maxT = self.profile.shape[0]        # Maximum time
+		maxX = self.profile[0, -1, 1]       # Maximum distance
+		maxY = self.profile[:, :, 2].max()  # Maximum value
+
+		def init():
+			ax = plt.axes(xlim=(0, maxX), ylim=(0, maxY))
+
+			l1, = ax.plot([], [])
+			plt.xlabel('Distance ($\sigma$)')
+			plt.ylabel(quantity)
+			plt.grid()
+			l1.set_data([], [])
+
+			return [l1]
+
+		[l1] = init()
+
+		def animate(t):
+			l1.set_data(self.profile[t, :, 1], self.profile[t, :, 2])
+			return [l1]
+
+		anim = animation.FuncAnimation(fig, animate, init_func=init, frames=maxT, interval=100, blit=True)
+
+		# Save plot
+		anim.save(directory + quantity + '.mp4', fps=30, bitrate=5000)
---
+++ LAMMPSDataParser.py
@@ -0,0 +1,114 @@
+"""
+Exports the LAMMPSDataParser class and Dims enum.
+"""
+
+from io import StringIO
+from typing import Sequence, Tuple, List
+import re
+from enum import Enum
+
+import numpy as np
+import pandas as pd
+
+
+class Dims(Enum):
+	x = 0
+	y = 1
+	z = 2
+
+
+class LAMMPSDataParser:
+	"""
+	Parses the position data of atoms from LAMMPS data files.
+	See https://lammps.sandia.gov/doc/write_data.html
+	"""
+	def __init__(self, filename: str):
+		"""
+		Parses box dimensions and atom position data from LAMMPS data file and loads it into a Pandas dataframe.
+		:param filename: Path to the LAMMPS data file.
+		"""
+		data_string = ""
+		box_dims: np.ndarray = np.empty((2, 3))
+		with open(filename) as f:
+			# Extract box dimensions using regex parsing
+			for dim in Dims:
+				for line in f:
+					p = re.compile(rf'([-+]?\d*\.?\d*[eE][+-]\d*) ([-+]?\d*\.?\d*[eE][+-]\d*) {dim.name}lo {dim.name}hi')
+					match = p.search(line)
+					if match:
+						# Found line, stop search for this dimension
+						box_dims[:, dim.value] = match.group(1, 2)
+						break
+
+			# Copy lines between the lines in the file delimiting position data
+			copy = False
+			for line in f:
+				# Beginning of position data
+				if line.strip() == "Atoms # bond":
+					# Skip first line
+					copy = True
+					continue
+				# End of position data
+				elif line.strip() == "Velocities":
+					break
+				elif copy:
+					if line.strip() == "":
+						continue
+					data_string += line
+
+		# Compute box sizes by taking difference of lo and hi values
+		self._box_sizes: np.ndarray = box_dims.ptp(axis=0)
+
+		# Put position data in Pandas dataframe
+		self._data: pd.DataFrame = pd.read_csv(StringIO(data_string), sep=' ', header=None, index_col=0, engine='c',
+		                                       names=['id', 'mol', 'type', 'x', 'y', 'z', 'nx', 'ny', 'nz'],
+		                                       dtype=[('id', int), ('mol', int), ('type', int),
+		                                              ('x', float), ('y', float), ('z', float),
+		                                              ('nx', int), ('ny', int), ('nz', int)])
+
+		# Subtract the lower box coordinates from all atom coordinates, i.e. put the origin (0, 0, 0) at the
+		# front-bottom-left corner so position coordinates go from 0 to box_size
+		for dim in Dims:
+			self._data[dim.name] -= box_dims[0, dim.value]
+
+	def get_positions_by_type(self, atom_types: Sequence[int]) -> np.ndarray:
+		"""
+		Selects atoms by type and returns the positions.
+		:param atom_types: List of integers corresponding to the atom types that should be included.
+		:return: A 2D ndarray with shape (N, 3) where N is the number of atoms and the 3
+		columns corresponds to the X, Y, and Z coordinates.
+		"""
+		return self._data.loc[self._data['type'].isin(atom_types), ['x', 'y', 'z']].to_numpy()
+
+	def get_density_profile(self, atom_types: Sequence[int], dimensions: Sequence[str], resolution: float)\
+		-> Tuple[np.ndarray, List[np.ndarray]]:
+		"""
+		Computes a N-dimensional density from the atom position data.
+		:param atom_types: List of integers corresponding to the atom types that should be included in the total
+		                   density.
+		:param dimensions: List of strings corresponding to the dimensions over which the density should be profiled.
+		                   The resulting density will be averaged over the remaining densities. For example,
+		                   when dimensions = ['z'], a 1D density profile in Z will be obtained, averaged over X and Y.
+		:param resolution: The linear resolution (in units of distance) of the density profile.
+		:return (density, bin_locations): A N-dimensional ndarray containing the density and a list of N 1D ndarrays
+		        describing the axes (spatial bin locations) for every dimension.
+		"""
+		# Get dimension ids from names
+		dim_ids = [Dims[dim].value for dim in dimensions]
+		# Calculate number of bins and spatial ranges for every dimension
+		n_bins = [int(self._box_sizes[dim_num] / resolution) for dim_num in dim_ids]
+		ranges = [(0, self._box_sizes[dim_num]) for dim_num in dim_ids]
+
+		# Get atom positions and convert to density by taking a histogram
+		atom_positions = self.get_positions_by_type(atom_types)[:, dim_ids]
+		(density, bins_edges) = np.histogramdd(atom_positions, bins=n_bins, range=ranges)
+
+		# Convert the N+1 bin edges to N midpoint values
+		bin_locations = [bin_edges[1:] - resolution/2 for bin_edges in bins_edges]
+
+		# Calculate the (3D) bin volume and divide the density array by it to normalise
+		mean_dim_ids = np.setdiff1d([dim.value for dim in Dims], dim_ids, assume_unique=True)
+		bin_volume = resolution**len(dimensions) * np.prod([self._box_sizes[dim_num] for dim_num in mean_dim_ids])
+		density /= bin_volume
+
+		return density, bin_locations
