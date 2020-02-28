8324dc268becf9c5bca18f96d680bcf201f65a68
lars <lars@tuxplace.nl>
2020-02-28 16:18:16 +0100

    Add bead_size and bottom_padding variables, use bead_id for the mol_id

--- BrushGenerators/KremerGrestBrushGenerator.py
+++ BrushGenerators/KremerGrestBrushGenerator.py
@@ -1,7 +1,7 @@
 """
 Exports the KremerGrestBrushGenerator class
 """
-
+from typing import Tuple, Optional
 from enum import Enum
 
 import numpy as np
@@ -25,18 +25,27 @@ class KremerGrestBrushGenerator(BrushGenerator):
 		AtomTypes.solvent: 1
 	}
 
+	def __init__(self, box_size: Tuple[float, float, float], rng_seed: Optional[int]):
+		"""
+		:param Tuple box_size:  3-tuple of floats describing the dimensions of the rectangular box.
+		:param int   rng_seed:  Seed used to initialize the PRNG. May be None, in which case a random seed will be used.
+		"""
+		bead_size = 1       # (sigma)
+		bottom_padding = 1  # (sigma)
+		super().__init__(box_size, rng_seed, bead_size, bottom_padding)
+
 	def _build_bead(self, mol_id: int, graft_coord: np.ndarray, bead_id: int) -> None:
 		if bead_id == 0:
 			atom_type = self.AtomTypes.graft.value
 		else:
 			atom_type = self.AtomTypes.bead.value
 
-		self._atoms_list.append({'mol_id':    mol_id + 1,
+		self._atoms_list.append({'mol_id':    bead_id + 1,
 		                         'atom_type': atom_type,
 		                         'q':         0,
 		                         'x':         graft_coord[0],
 		                         'y':         graft_coord[1],
-		                         'z':         float(bead_id) + 1
+		                         'z':         float(bead_id * self.bead_size)
 		                         })
 
 		# Molecular topology
